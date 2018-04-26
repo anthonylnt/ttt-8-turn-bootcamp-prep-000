ed1490e3479dd2595503fbeb6128abd9e03e9d78
Anthony Elijah <lionnet.ant@gmail.com>
2018-04-26 04:49:29 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -19,9 +19,11 @@ end
 
 def valid_move?(board, index)
   if index.between?(0,8) && !position_taken?(board, index)
+    return true
     move(board, index)
     display_board(board)
   else
+    return false
     turn(board)
   end
 end
