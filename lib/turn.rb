3aa3761a3c2ea27e9faadfbe3056b8e7c13478f4
Anthony Elijah <lionnet.ant@gmail.com>
2018-04-26 04:34:31 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -27,7 +27,7 @@ def valid_move?(index, board)
 end
 
 def position_taken?(board, index)
-  if board[index].include? "O" || board[index].include? "O"
+  if board[index].include? "O" || board[index].include? "X"
     true
   else 
     false
